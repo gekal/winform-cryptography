34597f5454b65d46181b7b088a739d5d6c39fbcd
liu hongying <liu.hongying@hotmail.com>
2017-07-15 22:59:59 +0900

    始めのコミット

---
+++ Cryptography/AesCrypto.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Cryptography
+{
+    public class AesCrypto
+    {
+        //----------------------------------------------------------------------
+        // 暗号化する
+        //----------------------------------------------------------------------
+        public static string Encrypt(string input, string key, string iv)
+        {
+            var keyBytes = hexStringToByteArray(key);
+            var ivBytes = hexStringToByteArray(iv);
+
+            var rijndaelManaged = new RijndaelManaged
+            {
+                Mode = CipherMode.CBC,
+                Padding = PaddingMode.PKCS7,
+                KeySize = 128,
+                BlockSize = 128,
+                Key = keyBytes,
+                IV = ivBytes
+            };
+            input = input.Replace("\r\n", "\n");
+            var plainBytes = Encoding.UTF8.GetBytes(input);
+            var bytes = rijndaelManaged.CreateEncryptor().TransformFinalBlock(plainBytes, 0, plainBytes.Length);
+            var base64Text = Convert.ToBase64String(bytes);
+            
+            return InsertLineBreaks(base64Text);
+        }
+
+        public static String InsertLineBreaks(String s)
+        {
+            int len = s.Length;
+            var data = new StringBuilder(len + len / 64);
+
+            for (int i = 0; i < len; i += 64)
+            {
+                if (i + 64 < len)
+                {
+                    data.Append(s.Substring(i, 64)); data.Append("\n");
+                }
+                else
+                {
+                    data.Append(s.Substring(i));
+                }
+            }
+
+            return data.ToString();
+        }
+
+        //----------------------------------------------------------------------
+        // 復号する
+        //----------------------------------------------------------------------
+        public static string Decrypt(string input, string key, string iv)
+        {
+            var keyBytes = hexStringToByteArray(key);
+            var ivBytes = hexStringToByteArray(iv);
+
+            var rijndaelManaged = new RijndaelManaged
+            {
+                Mode = CipherMode.CBC,
+                Padding = PaddingMode.PKCS7,
+                KeySize = 128,
+                BlockSize = 128,
+                Key = keyBytes,
+                IV = ivBytes
+            };
+            byte[] cipheredBytes = Convert.FromBase64String(input);
+            var bytes = rijndaelManaged.CreateDecryptor().TransformFinalBlock(cipheredBytes, 0, cipheredBytes.Length);
+            return Encoding.UTF8.GetString(bytes);
+        }
+        
+        public static byte[] hexStringToByteArray(String s)
+        {
+            int len = s.Length;
+            byte[] data = new byte[len / 2];
+            for (int i = 0; i < len; i += 2)
+            {
+                data[i / 2] = Convert.ToByte(s.Substring(i, 2), 16);
+            }
+            return data;
+        }
+    }
+}
---
+++ Cryptography/MainWindow.xaml.cs
@@ -0,0 +1,94 @@
+﻿using Cryptography.Properties;
+using Microsoft.Win32;
+using System.ComponentModel;
+using System.IO;
+using System.Windows;
+
+namespace Cryptography
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window, INotifyPropertyChanged
+    {
+
+        #region "モデル"
+        /// <summary>
+        /// 暗号化キー
+        /// </summary>
+        public string key { get; set; }
+
+        /// <summary>
+        ///  ベクトル
+        /// </summary>
+        public string iv { get; set; }
+
+        /// <summary>
+        ///  バリュー
+        /// </summary>
+        public string value { get; set; }
+
+        /// <summary>
+        ///  ファイル名
+        /// </summary>
+        public string filePath { get; set; }
+        #endregion
+
+        #region "コンストラクタ"
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = this;
+
+            key = Settings.Default.key;
+            iv = Settings.Default.iv;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        #endregion
+
+        /// <summary>
+        /// オープンのボタンのクリックイベント
+        /// </summary>
+        /// <param name="sender">sender</param>
+        /// <param name="e">e</param>
+        private void openBtn_Click(object sender, RoutedEventArgs e)
+        {
+            var fileDialog = new OpenFileDialog();
+            var result = fileDialog.ShowDialog();
+            if (result == true)
+            {
+                filePath = fileDialog.FileName;
+
+                value = AesCrypto.Decrypt(File.ReadAllText(filePath), key, iv);
+                PropertyChanged(this, new PropertyChangedEventArgs("value"));
+                saveBtn.IsEnabled = true;
+            }
+        }
+
+        /// <summary>
+        /// 保存のボタンのクリックイベント
+        /// </summary>
+        /// <param name="sender">sender</param>
+        /// <param name="e">e</param>
+        private void saveBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+            try
+            {
+                var encryptedText = AesCrypto.Encrypt(value, key, iv);
+                File.WriteAllText(filePath, encryptedText);
+
+                MessageBox.Show("暗号化処理が成功しました。", "暗号化", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+            catch
+            {
+                MessageBox.Show("暗号化処理が失敗しました。", "暗号化", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+    }
+}
